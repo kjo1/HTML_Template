f89398515c5c8abef4a65ad2db6302f521b92449
Kang <kjo0@outlook.com>
2019-01-20 21:39:52 -0500

    Project Added

---
+++ HTML_Template/HTML_Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTML_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
